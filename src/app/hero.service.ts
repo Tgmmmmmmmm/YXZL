0ab6d0113577b937061ea0704ccc9025f51b26ee
Tgmmmmmmmm <2282680112@qq.com>
2021-06-21 12:03:27 +0800

    complete serve

---
+++ src/app/hero.service.ts
@@ -0,0 +1,40 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Hero } from './hero';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+// 请留意修改为你自己的URL
+  private heroesUrl = 'http://localhost:2403/heroes';
+// 注入了HttpClient服务
+  constructor(private httpClient: HttpClient) { }
+// 获取所有英雄
+  getHeroes(): Observable<Hero[]> {
+    return this.httpClient.get<Hero[]>(this.heroesUrl);
+  }
+// 获取指定id的英雄
+  getHeroById(id: string): Observable<Hero> {
+    return this.httpClient.get<Hero>(`${this.heroesUrl}/${id}`);
+  }
+// 获取所有的顶级英雄
+  getTopHeroes(): Observable<Hero[]> {
+    // 请参考Deployd的API文档
+    return this.httpClient.get<Hero[]>(`${this.heroesUrl}?isTop=true`);
+  }
+// 添加一个英雄
+  addHero(hero: Hero): Observable<Hero> {
+    return this.httpClient.post<Hero>(this.heroesUrl, hero);
+  }
+// 删除一个英雄
+  deleteHero(id: string): Observable<any> {
+    return this.httpClient.delete(`${this.heroesUrl}/${id}`);
+  }
+// 更新一个英雄
+  updateHero(hero: Hero): Observable<Hero> {
+    return this.httpClient.put<Hero>(this.heroesUrl, hero);
+  }
+}
---
+++ src/app/hero.ts
@@ -0,0 +1,11 @@
+
+export class Hero {
+    constructor(
+      public id: string,
+      public no: string,
+      public name: string,
+      public description?: string,
+      public isTop?: boolean
+    ) {}
+  }
+  
