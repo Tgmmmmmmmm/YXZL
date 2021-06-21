f8b8dd69838000cf54f691e17163edfd2f864fa5
Tgmmmmmmmm <2282680112@qq.com>
2021-06-22 02:27:41 +0800

    列表初步

--- src/app/components/hero-list/hero-list.component.ts
+++ src/app/components/hero-list/hero-list.component.ts
@@ -2,14 +2,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../hero';
 import { HeroService } from '../../hero.service';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-hero-list',
   templateUrl: './hero-list.component.html',
-  styleUrls: ['./hero-list.component.scss']
+  styleUrls: ['./hero-list.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+    state('collapsed', style({height: '0px', minHeight: '0'})),
+    state('expanded', style({height: '*'})),
+    transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+    ],
 })
 export class HeroListComponent implements OnInit {
   heroes: Hero[] = [];
+  columnsToDisplay = ['no', 'name', 'top'];
+  expandedElement!: Hero ;
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
@@ -27,3 +37,8 @@ export class HeroListComponent implements OnInit {
     console.log("s")
   }
 }
+
+
+
+
+
